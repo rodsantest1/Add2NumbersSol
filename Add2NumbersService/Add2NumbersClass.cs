7a12ebf589641b56c338acf7ca69a23bccdebcf6
Rodney Santiago <rodney.santiago@ups.com>
2022-11-02 13:47:23 -0400

    Finished important business. Done.

---
+++ Add2NumbersService/Add2NumbersClass.cs
@@ -0,0 +1,18 @@
+﻿namespace Add2NumbersService
+{
+    public class Add2NumbersClass
+    {
+        public async Task<int> Add2NumbersFunction(string input1, string input2)
+        {
+            return await Task.Run(() =>
+            {
+                int.TryParse(input1, out int number1);
+                int.TryParse(input2, out int number2);
+
+                var sum = number1 + number2;
+
+                return sum;
+            });
+        }
+    }
+}
